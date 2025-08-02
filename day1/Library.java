eb76732d16cdcb2ff9ad88b4bcfcca2cb14a0f2c
Praveen-Jayasundram <Praveen.jayasundram@gmail.com>
2025-08-02 16:23:01 +0530

    Add files via upload
    week 2 day 1

---
+++ day1/Library.java
@@ -0,0 +1,20 @@
+package week2.day1;
+
+public class Library {
+
+	public String addBook(String bookTitle) {
+		System.out.println(bookTitle + ": Book added successfully");
+		return ("bookTitle");
+	}
+
+	public void issueBook() {
+		System.out.println("Book issued successfully");
+	}
+
+	public static void main(String[] args) {
+		Library book = new Library();
+		book.addBook("Harrypotter");
+		book.issueBook();
+	}
+
+}
---
+++ day1/LibraryManagement.java
@@ -0,0 +1,13 @@
+package week2.day1;
+
+public class LibraryManagement {
+	
+	public static void main(String[] args)
+	{
+		Library book = new Library();
+		book.addBook("Harrypotter");
+		book.issueBook();
+
+	}
+
+}
